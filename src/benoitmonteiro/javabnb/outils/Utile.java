018cb3bffeb648bbb77956debebe68a82ec307f1
Benoit-Monteiro-Da-Silva <benoit.monteirods@gmail.com>
2026-01-08 12:15:43 +0100

    correted Utile methods

--- src/benoitmonteiro/javabnb/outils/Utile.java
+++ src/benoitmonteiro/javabnb/outils/Utile.java
@@ -5,9 +5,15 @@ import java.util.Date;
 
 public class Utile {
 
+    /**
+     * @param jour int between 1 and 31
+     * @param mois int between 1 and 12
+     * @param annee int
+     * @return Date object
+     */
     public static Date createDate(int jour, int mois, int annee) {
         
-        return new Date(annee, mois - 1, jour);
+        return new Date(annee - 1900, mois - 1, jour);
 
     }
 
@@ -16,8 +22,8 @@ public class Utile {
         String date = (dateObj.getDate() <= 9) ? "0" + dateObj.getDate() : "" + dateObj.getDate();
         int correctedMonth = dateObj.getMonth() + 1;
         String month = (correctedMonth <= 9) ? "0" + correctedMonth : "" + correctedMonth;
-        String year = "" + dateObj.getYear();
-        String dateFormatee = date + "/" + month + "/" + year;
+        int correctedYear = dateObj.getYear() + 1900;
+        String dateFormatee = date + "/" + month + "/" + correctedYear;
 
         return dateFormatee;
 
