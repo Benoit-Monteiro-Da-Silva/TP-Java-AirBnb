0ac27ab7d36b81bcccce39e47adc296db8182b4b
Benoit-Monteiro-Da-Silva <benoit.monteirods@gmail.com>
2026-01-13 16:55:25 +0100

    use Singleton

--- src/benoitmonteiro/javabnb/Main.java
+++ src/benoitmonteiro/javabnb/Main.java
@@ -3,11 +3,9 @@ package benoitmonteiro.javabnb;
 import java.util.ArrayList;
 import java.util.Date;
 
-import benoitmonteiro.javabnb.outils.LogementsDomParser;
+import benoitmonteiro.javabnb.outils.JavaBnBData;
 import benoitmonteiro.javabnb.outils.Utile;
-import benoitmonteiro.javabnb.logements.Appartement;
 import benoitmonteiro.javabnb.logements.Logement;
-import benoitmonteiro.javabnb.logements.Maison;
 import benoitmonteiro.javabnb.utilisateurs.Voyageur;
 import benoitmonteiro.javabnb.utilisateurs.Hote;
 import benoitmonteiro.javabnb.reservations.Sejour;
@@ -19,58 +17,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Jeu de données perso
-        Voyageur alain = new Voyageur("Alain", "Dupont", 28);
-        Hote justine = new Hote("Justine", "Cerise", 19, 3 );
+        //Récupération des données (logements et hôtes)
+        JavaBnBData data = JavaBnBData.getInstance();
+        ArrayList<Logement> logements = data.getLogements();
+        ArrayList<Hote> hotes = data.getHotes();
 
-        Maison chezMoi = new Maison(
-            justine, 
-            80, 
-            "14 rue Colbert, 37000 Tours", 
-            70, 
-            4, 
-            50, 
-            true);
+        //Création d'un voyageur
+        Voyageur alain = new Voyageur("Alain", "Durant", 31);
 
-        Appartement chezLui = new Appartement(
-            justine, 
-            50,
-            "7 rue du Grenier, 41000 Blois", 
-            25, 
-            2, 
-            0, 
-            4);
-
-
-        //Creation de listes
-        ArrayList<Logement> listeDeLogements = new ArrayList<>();
-        ArrayList<Hote> listeDHotes = new ArrayList<>();
-
-        //Tentative de récupération des logements et des hôtes du fichier res/logements.xml dans listeDeLogements et listeDHotes
-        try {
-            LogementsDomParser.parse("res/logements.xml", listeDHotes, listeDeLogements);
-        } catch (Exception e) {
-            System.out.println("Erreur lors de la récupération des données.");
-        }
-
-        //Affichage de la liste d'hôtes
-        for (Hote hote : listeDHotes) {
-            hote.afficher();
-        }
-
-        
         //Création d'un séjour
         Date dateSejour = Utile.createDate(25, 8, 2026);
         int nbNuits = 10;
         int nbVoyageurs = 2;
-        Logement logementVacances = chezLui;
-        Sejour lesVacances;
-
-        lesVacances = SejourFactory.creerSejour(dateSejour, nbNuits, nbVoyageurs, logementVacances);
+        Logement logementVacances = logements.get(0);
+        Sejour lesVacances = SejourFactory.creerSejour(dateSejour, nbNuits, nbVoyageurs, logementVacances);
 
         try {
             Reservation lesVacancesReservees = new Reservation(lesVacances, alain);
-            // lesVacancesReservees.afficher();
+            lesVacancesReservees.afficher();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             System.out.println("La création de la réservation a échoué.");
